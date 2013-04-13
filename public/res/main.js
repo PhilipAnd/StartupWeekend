b0b63ae0c1d798c7883f1b30fe7dba9d37080827
Philip Andersen <philipmandersen@gmail.com>
2013-04-13 22:20:41 +0200

    Added footer

---
+++ public/res/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  loadFrontPage();
+
+  setTimeout(function(){placeFooter()}, 3000);
+  $(window).resize(function(){
+    placeFooter();
+  });
+
+  $('#aFrontPage').click(function(){
+    loadFrontPage();
+  });
+
+  $('#aAdsPage').click(function(){
+    loadAdsPage();
+  });        
+});
+
+function loadFrontPage(){
+  if($('#divAdsPage').is(':visible'))
+  {
+    $('#divAds').ads('destroy');
+  }
+  $('#divAdsPage').hide();
+  $('#divFrontPage').show();
+  $('#ulAdvertisers').approveadvertisers({
+    templateSelector: '#advertiserItemTmpl',
+    approveSelector: '.js_approveAdvertiser',
+    disapproveSelector: '.js_disapproveAdvertiser'
+  });
+};
+
+function placeFooter()
+{
+  var footer = $('#modalAlreadyFiltered');
+  var wHeight = $(window).height();
+  var footerHeight = footer.height();
+  var offset = parseInt(wHeight) - parseInt(footerHeight);
+  footer.css('top',offset);
+  footer.fadeIn();
+};
+
+function loadAdsPage()
+{
+  $('#ulAdvertisers').approveadvertisers('destroy');
+  $('#divAdsPage').show();
+  $('#divFrontPage').hide();
+  $('#divAds').ads({
+      templateSelector: '#adItemTmpl',
+      approveSelector: '.adHover .add',
+      disapproveSelector: '.adHover .remove'
+    });
+};
+
+function closeOverlay(){
+  $('#modalAlreadyFiltered').fadeOut();
+};
