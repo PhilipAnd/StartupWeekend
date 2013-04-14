911420b868ba5db4196910331e01af5f4d2d8d89
Philip Andersen <philipmandersen@gmail.com>
2013-04-14 11:40:41 +0200

    Added hack for events

--- public/res/main.js
+++ public/res/main.js
@@ -1,3 +1,6 @@
+var isFrontPageLoaded = false;
+var isAdsPageLoaded = false;
+
 $(document).ready(function(){
   loadFrontPage();
 
@@ -20,20 +23,6 @@ $(document).ready(function(){
   });        
 });
 
-function loadFrontPage(){
-  if($('#divAdsPage').is(':visible'))
-  {
-    $('#divAds').ads('destroy');
-  }
-  $('#divAdsPage').hide();
-  $('#divFrontPage').show();
-  $('#ulAdvertisers').approveadvertisers({
-    templateSelector: '#advertiserItemTmpl',
-    approveSelector: '.js_approveAdvertiser',
-    disapproveSelector: '.js_disapproveAdvertiser'
-  });
-};
-
 function placeFooter()
 {
   var footer = $('#modalAlreadyFiltered');
@@ -44,16 +33,33 @@ function placeFooter()
   footer.fadeIn('3000');
 };
 
+function loadFrontPage(){
+  $('#divAdsPage').hide();
+  $('#divFrontPage').show();
+  if(!isFrontPageLoaded)
+  {
+    $('#ulAdvertisers').approveadvertisers({
+      templateSelector: '#advertiserItemTmpl',
+      approveSelector: '.js_approveAdvertiser',
+      disapproveSelector: '.js_disapproveAdvertiser'
+    });
+    isFrontPageLoaded = true;
+  }
+};
+
 function loadAdsPage()
 {
-  $('#ulAdvertisers').approveadvertisers('destroy');
   $('#divAdsPage').show();
   $('#divFrontPage').hide();
-  $('#divAds').ads({
-      templateSelector: '#adsItemTmpl',
-      approveSelector: '.adHover .add',
-      disapproveSelector: '.adHover .remove'
-    });
+  if(!isAdsPageLoaded)
+  {
+    $('#divAds').ads({
+        templateSelector: '#adsItemTmpl',
+        approveSelector: '.adHover .add',
+        disapproveSelector: '.adHover .remove'
+      });
+    isAdsPageLoaded = true;
+  }
 };
 
 function closeOverlay(){
